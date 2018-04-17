b686bfbfb3f8a1fca61f4849724a951dbaba3c3c
liuwenhui <2679277664.qq.com>
2018-04-17 09:24:57 +0800

    lwh1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// require.config({
+//     baseUrl: 'js/lib',
+//     paths: {
+//         'jQuery': './jquery-1.8.3.min.js'
+//     }
+// })
+
+
+// define('js', function($) {
+
+// })
+
+function require() {
+
+}
+
+
+function define() {
+
+}
